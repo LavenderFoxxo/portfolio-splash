114c9c1266771227a1fb9df5826fa62c9e84293b
Alexander Hyman <alexander.hyman05@gmail.com>
2023-01-26 21:41:19 -0800

    fix: plz work static images

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 // Routes
-app.use(express.static('public'))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/', Main)
 
 // Are u listening bruh?
